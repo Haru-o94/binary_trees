143544e6f5bfdf2e1934cac9ce5b2f5cccdc8cc5
= <=>
2024-03-26 10:40:41 +0300

    update 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -4,6 +4,7 @@
  * binary_tree_leaves - leave of binary tree
  *
  * @tree: If tree is NULL, the function must return 0
+ * Return: null or tree
  */
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
